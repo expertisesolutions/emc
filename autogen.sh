8b7e4c5fee9d11d6ec96aa2c695f098e47c0a2ce
Savio Sena <savio.sena@acm.org>
2014-09-17 07:37:24 -0300

    autotools: Cleanup autogen.sh comments.

--- autogen.sh
+++ autogen.sh
@@ -3,15 +3,10 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
-# aclocal && autoscan && autoconf
-# autoreconf -vif
-#echo "Running autopoint..." ; autopoint -f || :
-#echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-#echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 W=0
 
